9473b3af94c70e50073b360c05c3170e95acc221
davialvd <davidalvaradodu@gmail.com>
2021-03-04 09:47:30 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [AppComponent,NavbarComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+  apiKey: "AIzaSyDtSo0rWc_LYObopQ8VZXtKmpTtEdmUihI",
+  authDomain: "login-ng-ad241.firebaseapp.com",
+  projectId: "login-ng-ad241",
+  storageBucket: "login-ng-ad241.appspot.com",
+  messagingSenderId: "468586228135",
+  appId: "1:468586228135:web:e738a6d17fb27a01cc6e82"
+},
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
